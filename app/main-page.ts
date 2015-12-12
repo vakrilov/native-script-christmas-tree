34407327da841a564293bb397fdd57768fb51d21
vakrilov <alexander.vakrilov@telerik.com>
2015-12-12 19:05:50 +0200

    Finish animations

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,11 +1,8 @@
 import observable = require("data/observable");
 import pages = require("ui/page");
 import gestures = require("ui/gestures");
-import {Image} from "ui/image";
-import {View} from "ui/core/view";
-import {Label} from "ui/label";
-import {LayoutBase} from "ui/layouts/layout-base";
-import {TextBase} from "ui/text-base";
+import {Image, View, Label, LayoutBase, TextBase, Button} from "ui";
+import {Color} from "color";
 import {screen} from "platform";
 import utils = require("utils/utils");
 
@@ -24,12 +21,21 @@ var selectedBall: BallWithChain;
 var selectedDropSpot: DropSpot;
 
 var initialized = false;
+var shareBtn: Button;
+var star: Image;
+
+interface Vector {
+    x: number,
+    y: number,
+    dist(other: Vector): number
+}
 
 interface DropSpot {
     x: number;
     y: number;
     view?: View;
     pos?: any;
+    ball?: BallWithChain;
 }
 
 var dropSpots: Array<DropSpot> = [
@@ -48,11 +54,13 @@ var ballsWithChains: Array<BallWithChain> = [
 
 // Event handler for Page "loaded" event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
-    DENSITY = utils.layout.getDisplayDensity();    
+    DENSITY = utils.layout.getDisplayDensity();
     var page = <pages.Page>args.object;
 
     var container = <LayoutBase>page.getViewById("container");
     var metaText = <TextBase>page.getViewById("meta");
+    star = <Image>page.getViewById("star");
+    shareBtn = <Button>page.getViewById("btn-share");
 
     setTimeout(function() {
         initWrold(container, metaText);
@@ -78,7 +86,7 @@ function addDropSpots(container: LayoutBase) {
 function adjustToContainerSize(container: LayoutBase) {
     var width = container.getMeasuredWidth() / DENSITY;
     var height = container.getMeasuredHeight() / DENSITY;
-    
+
     var offsetX = (width - SCENE_WIDTH) / 2;
     var offsetY = (height - SCENE_HEIGHT - 60) / 2; // 60 - hieght of the share button
     
@@ -105,83 +113,101 @@ function initWrold(container: LayoutBase, metaText: TextBase) {
     initialized = true;
 
     adjustToContainerSize(container);
-    
+
     addDropSpots(container);
-    
+
     initPhysicsWorld(container, metaText, ballsWithChains)
 }
 
-
-
-export function onPan(args: gestures.PanGestureEventData) {
-    let touchX: number;
-    let touchY: number;
+function getTouchPosition(args: gestures.PanGestureEventData): Vector {
+    var result: Vector;
     if (args.android) {
-        touchX = args.android.current.getX() / DENSITY;
-        touchY = args.android.current.getY() / DENSITY;
+        result = new Physics.vector(args.android.current.getX() / DENSITY, args.android.current.getY() / DENSITY);
     }
     else if (args.ios) {
         var pos = args.ios.locationInView((<View>args.object).ios);
-        touchX = pos.x;
-        touchY = pos.y;
+        result = new Physics.vector(pos.x, pos.y);
     }
+    return result;
+}
 
-    if (touchX === undefined || touchY === undefined) {
+export function onPan(args: gestures.PanGestureEventData) {
+    var touch: Vector = getTouchPosition(args);
+    if (!touch) {
         return;
     }
-
-    var touch = new Physics.vector(touchX, touchY);
-    // console.log(`PAN state: ${ args.state } touch: ${ touch.toString() }`);
-
-    if (args.state === 1) { // gesture begin
+    if (args.state === 1) {
         for (var bwc of ballsWithChains) {
             if (touch.dist(bwc.anchorRef.state.pos) < 20) {
-                selectedBall = bwc;
+                startDragging(bwc);
                 break;
             }
         }
-        if (selectedBall) {
-            selectedBall.anchorRef.view.scaleX = 2;
-            selectedBall.anchorRef.view.scaleY = 2;
-        }
     }
-    else if (args.state === 3 && selectedBall) { // gesture end
-        if (selectedDropSpot) {
-            selectedDropSpot.view.scaleX = 1;
-            selectedDropSpot.view.scaleY = 1;
+    else if (args.state === 3 && selectedBall) {
+        endDragging();
+    }
+    if (selectedBall) {
+        drag(touch);
+    }
+}
+
+function startDragging(ball: BallWithChain) {
+    selectedBall = ball;
+    selectedBall.anchorRef.view.scaleX = 2;
+    selectedBall.anchorRef.view.scaleY = 2;
+    var unfinish = false;
+    dropSpots.forEach(function(spot) {
+        if (spot.ball === selectedBall) {
+            spot.ball = undefined;
+            unfinish = true;
+        }
+    });
+    
+    if(unfinish){
+        animateButton(false);
+    }
+}
+
+function endDragging() {
+    if (selectedDropSpot) {
+        selectedDropSpot.view.scaleX = 1;
+        selectedDropSpot.view.scaleY = 1;
+        selectedDropSpot.ball = selectedBall;
+    }
+    selectedBall.anchorRef.view.scaleX = 1;
+    selectedBall.anchorRef.view.scaleY = 1;
+    selectedBall = undefined;
+    selectedDropSpot = undefined;
+
+    if (dropSpots.every((spot) => !!spot.ball)) {
+        animateStar();
+        animateButton(true);
+    }
+}
+
+function drag(touch: Vector) {
+    for (var dropSpot of dropSpots) {
+        if (dropSpot.ball) {
+            //slot taken - move on;
+            continue;
+        }
+        if (touch.dist(dropSpot.pos) < 10) {
+            selectedDropSpot = dropSpot;
+            selectedDropSpot.view.scaleX = 2.2;
+            selectedDropSpot.view.scaleY = 2.2;
+            touch.x = dropSpot.x;
+            touch.y = dropSpot.y;
         }
         else {
-            //reset
-            //selectedBall.anchorRef.state.pos.set(selectedBall.anchorX, selectedBall.anchorY);
-        }
-
-        selectedBall.anchorRef.view.scaleX = 1;
-        selectedBall.anchorRef.view.scaleY = 1;
-        selectedBall = undefined;
-        selectedDropSpot = undefined;
-    }
-
-    if (selectedBall) {
-        for (var dropSpot of dropSpots) {
-            if (touch.dist(dropSpot.pos) < 10) {
-                selectedDropSpot = dropSpot;
-                selectedDropSpot.view.scaleX = 2.2;
-                selectedDropSpot.view.scaleY = 2.2;
-
-                touchX = dropSpot.x;
-                touchY = dropSpot.y;
-            }
-            else {
-                dropSpot.view.scaleX = 1;
-                dropSpot.view.scaleY = 1;
-                if (selectedDropSpot === dropSpot) {
-                    selectedDropSpot = undefined;
-                }
+            dropSpot.view.scaleX = 1;
+            dropSpot.view.scaleY = 1;
+            if (selectedDropSpot === dropSpot) {
+                selectedDropSpot = undefined;
             }
         }
-
-        selectedBall.anchorRef.state.pos.set(touchX, touchY);
     }
+    selectedBall.anchorRef.state.pos.set(touch.x, touch.y);
 }
 
 export function onShare() {
@@ -189,14 +215,17 @@ export function onShare() {
     // Todo use screenshot && social share plugins to share
 }
 
-var animating:boolean = false;
+var animating: boolean = false;
 export function onStarTap(args) {
-    if(animating){
+    animateStar();
+}
+
+function animateStar() {
+    if (animating) {
         return;
     }
     animating = true;
-    var star = <View>args.object;
-   
+
     star.animate({
         rotate: 720,
         duration: 1500,
@@ -204,5 +233,10 @@ export function onStarTap(args) {
     }).then(() => {
         star.rotate = 0;
         animating = false;
-    })   
+    })
+}
+
+function animateButton(completed: boolean) {
+    var color = new Color(completed ? "#55acee" : "#808080");
+    shareBtn.animate({ backgroundColor: color });
 }
